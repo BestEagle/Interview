fa23b33e6f04cc91c16c9231c9726b730313203a
Alexander <sanek.bel@mail.ru>
2021-01-25 22:09:12 +0300

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {1, 4, 6};
+        int[] array = {1,7,9,12};
 
         System.out.println(generateNumber(array));
     }
@@ -20,15 +20,17 @@ public class Main {
 
         if (array.length == 1 && array[0] != 1)
             skip = array[0] - 1;
-        else skip = 2;
+        else skip = array.length + 1;
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i + 1] - array[i] > 1) {
                 skips.add(array[i + 1] - array[i]);
                 skip = Collections.min(skips);
-            } else skip = array.length + 1;
+            }
         }
 
-        return String.format("%1$" + 3 + "s", skip).replace(' ', '0');
+       return String.format("%1$" + 3 + "s", skip).replace(' ', '0');
+
+
     }
 }
 
